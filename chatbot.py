27f43f47eea05ae98a19a70a867af7eb73e2608f
abeeha <rabianasirbutt@gmail.com>
2026-05-09 10:16:46 +0100

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+print("hello! I am an AI Bot. What is your name ?")
+name = input()
+print(f"nice to meet you , {name}!")
+print("how are you feeling today ? (good / bad)")
+mood = input().lower()
+if mood == "good":
+    print("i am glad to hear that!")
+elif mood == "bad":
+    print("im sorry to hear that ! i hope you feel better soon")
+else:
+    print("i see . sometimes it is really hard to put your felling into words")
+print("What is the weather like today")
+weather = input().lower()
+if weather == "sunny":
+    print("THAT SOUND LIKE GOOD WEATHER !!")
+elif weather == "rainy":
+    print(i hope the weather gets better soon)
+else :
+    print("I see ")
+print("what are you going to do today")
+act = input()
+print(f"{name} that sounds quite fun")
+print("OKay i hope you have a good day today")
+print("BYEEE")
